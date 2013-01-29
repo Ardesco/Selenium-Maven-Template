c361b0ee51266b03d39bb72663122feb7edaaf47
Mark Collin <mark.collin@lazeryattack.com>
2013-01-29 20:21:50 +0000

    Add support for ghostdriver and remote into BrowserType enum

--- src/test/java/com/lazerycode/selenium/BrowserType.java
+++ src/test/java/com/lazerycode/selenium/BrowserType.java
@@ -1,20 +1,22 @@
 package com.lazerycode.selenium;
 
 public enum BrowserType {
-  FIREFOX("firefox"),
-  CHROME("chrome"),
-  IE("ie"),
-  SAFARI("safari"),
-  OPERA("opera"),
-  HTMLUNIT("htmlunit");
+    FIREFOX("firefox"),
+    CHROME("chrome"),
+    IE("ie"),
+    SAFARI("safari"),
+    OPERA("opera"),
+    GHOSTDRIVER("ghostdriver"),
+    REMOTE("remote"),
+    HTMLUNIT("htmlunit");
 
-  private final String browser;
+    private final String browser;
 
-  BrowserType(String browser) {
-    this.browser = browser;
-  }
+    BrowserType(String browser) {
+        this.browser = browser;
+    }
 
-  public String getBrowser() {
-    return browser;
-  }
+    public String getBrowser() {
+        return browser;
+    }
 }
