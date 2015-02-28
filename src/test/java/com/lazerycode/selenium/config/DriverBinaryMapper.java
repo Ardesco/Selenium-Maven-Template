e00684b5f6df2bebc62a7e10d891179ca0a9b3f1
Mark Collin <mark.collin@lazeryattack.com>
2015-02-28 16:13:24 +0200

    Update Selenium to 2.45.0 and remove presto Opera support Update to driver-binary-downloader-maven-plugin 1.0.5

--- src/test/java/com/lazerycode/selenium/config/DriverBinaryMapper.java
+++ src/test/java/com/lazerycode/selenium/config/DriverBinaryMapper.java
@@ -12,20 +12,20 @@ import static com.lazerycode.selenium.config.SystemArchitecture.ARCHITECTURE_64_
 
 public class DriverBinaryMapper {
     private static final Map<DriverBinaryContext, String> binaryLocation = new HashMap<DriverBinaryContext, String>() {{
-        put(binaryFor(CHROME, WINDOWS, ARCHITECTURE_64_BIT), binaryPath("/windows/googlechrome/64bit/2.10/chromedriver.exe"));
-        put(binaryFor(CHROME, MAC, ARCHITECTURE_64_BIT), binaryPath("/osx/googlechrome/64bit/2.10/chromedriver"));
-        put(binaryFor(CHROME, LINUX, ARCHITECTURE_64_BIT), binaryPath("/linux/googlechrome/64bit/2.10/chromedriver"));
-        put(binaryFor(CHROME, WINDOWS, ARCHITECTURE_32_BIT), binaryPath("/windows/googlechrome/32bit/2.10/chromedriver.exe"));
-        put(binaryFor(CHROME, MAC, ARCHITECTURE_32_BIT), binaryPath("/osx/googlechrome/32bit/2.10/chromedriver"));
-        put(binaryFor(CHROME, LINUX, ARCHITECTURE_32_BIT), binaryPath("/linux/googlechrome/32bit/2.10/chromedriver"));
-        put(binaryFor(DriverType.PHANTOMJS, WINDOWS, ARCHITECTURE_64_BIT), binaryPath("/windows/phantomjs/64bit/1.9.2/phantomjs.exe"));
-        put(binaryFor(DriverType.PHANTOMJS, MAC, ARCHITECTURE_64_BIT), binaryPath("/osx/phantomjs/64bit/1.9.7/phantomjs"));
-        put(binaryFor(DriverType.PHANTOMJS, LINUX, ARCHITECTURE_64_BIT), binaryPath("/linux/phantomjs/64bit/1.9.7/phantomjs"));
-        put(binaryFor(DriverType.PHANTOMJS, WINDOWS, ARCHITECTURE_32_BIT), binaryPath("/windows/phantomjs/32bit/1.9.7/phantomjs.exe"));
-        put(binaryFor(DriverType.PHANTOMJS, MAC, ARCHITECTURE_32_BIT), binaryPath("/osx/phantomjs/32bit/1.9.7/phantomjs"));
-        put(binaryFor(DriverType.PHANTOMJS, LINUX, ARCHITECTURE_32_BIT), binaryPath("/linux/phantomjs/32bit/1.9.7/phantomjs"));
-        put(binaryFor(IE, WINDOWS, ARCHITECTURE_64_BIT), binaryPath("/windows/internetexplorer/64bit/2.42.0/IEDriverServer.exe"));
-        put(binaryFor(IE, WINDOWS, ARCHITECTURE_32_BIT), binaryPath("/windows/internetexplorer/32bit/2.42.0/IEDriverServer.exe"));
+        put(binaryFor(CHROME, WINDOWS, ARCHITECTURE_64_BIT), binaryPath("/windows/googlechrome/64bit/2.14/chromedriver.exe"));
+        put(binaryFor(CHROME, MAC, ARCHITECTURE_64_BIT), binaryPath("/osx/googlechrome/64bit/2.14/chromedriver"));
+        put(binaryFor(CHROME, LINUX, ARCHITECTURE_64_BIT), binaryPath("/linux/googlechrome/64bit/2.14/chromedriver"));
+        put(binaryFor(CHROME, WINDOWS, ARCHITECTURE_32_BIT), binaryPath("/windows/googlechrome/32bit/2.14/chromedriver.exe"));
+        put(binaryFor(CHROME, MAC, ARCHITECTURE_32_BIT), binaryPath("/osx/googlechrome/32bit/2.14/chromedriver"));
+        put(binaryFor(CHROME, LINUX, ARCHITECTURE_32_BIT), binaryPath("/linux/googlechrome/32bit/2.14/chromedriver"));
+        put(binaryFor(DriverType.PHANTOMJS, WINDOWS, ARCHITECTURE_64_BIT), binaryPath("/windows/phantomjs/64bit/2.0.0/phantomjs.exe"));
+        put(binaryFor(DriverType.PHANTOMJS, MAC, ARCHITECTURE_64_BIT), binaryPath("/osx/phantomjs/64bit/2.0.0/phantomjs"));
+        put(binaryFor(DriverType.PHANTOMJS, LINUX, ARCHITECTURE_64_BIT), binaryPath("/linux/phantomjs/64bit/1.9.8/phantomjs"));
+        put(binaryFor(DriverType.PHANTOMJS, WINDOWS, ARCHITECTURE_32_BIT), binaryPath("/windows/phantomjs/32bit/2.0.0/phantomjs.exe"));
+        put(binaryFor(DriverType.PHANTOMJS, MAC, ARCHITECTURE_32_BIT), binaryPath("/osx/phantomjs/32bit/2.0.0/phantomjs"));
+        put(binaryFor(DriverType.PHANTOMJS, LINUX, ARCHITECTURE_32_BIT), binaryPath("/linux/phantomjs/32bit/1.9.8/phantomjs"));
+        put(binaryFor(IE, WINDOWS, ARCHITECTURE_64_BIT), binaryPath("/windows/internetexplorer/64bit/2.45.0/IEDriverServer.exe"));
+        put(binaryFor(IE, WINDOWS, ARCHITECTURE_32_BIT), binaryPath("/windows/internetexplorer/32bit/2.45.0/IEDriverServer.exe"));
 
     }};
 
