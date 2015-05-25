f2e81af5719d0cfb132426fbc5f70d6b7a4c878f
Mark Collin <mark.collin@lazeryattack.com>
2015-05-25 15:55:46 +0100

    refactor and clean up code

--- src/test/java/com/lazerycode/selenium/config/DriverType.java
+++ src/test/java/com/lazerycode/selenium/config/DriverType.java
@@ -1,6 +1,5 @@
 package com.lazerycode.selenium.config;
 
-import org.openqa.selenium.Platform;
 import org.openqa.selenium.Proxy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -10,29 +9,21 @@ import org.openqa.selenium.opera.OperaDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
-import static java.lang.Boolean.getBoolean;
-import static org.openqa.selenium.Proxy.ProxyType.MANUAL;
 import static org.openqa.selenium.remote.CapabilityType.PROXY;
 
 public enum DriverType implements DriverSetup {
 
     FIREFOX {
-        public DesiredCapabilities getDesiredCapabilities() {
+        public DesiredCapabilities getDesiredCapabilities(Proxy proxySettings) {
             DesiredCapabilities capabilities = DesiredCapabilities.firefox();
-            if (proxyEnabled) {
-                capabilities.setCapability(PROXY, getSeleniumProxyDetails());
-            }
-            return capabilities;
+            return addProxySettings(capabilities, proxySettings);
         }
 
         public WebDriver getWebDriverObject(DesiredCapabilities capabilities) {
@@ -40,16 +31,13 @@ public enum DriverType implements DriverSetup {
         }
     },
     CHROME {
-        public DesiredCapabilities getDesiredCapabilities() {
+        public DesiredCapabilities getDesiredCapabilities(Proxy proxySettings) {
             DesiredCapabilities capabilities = DesiredCapabilities.chrome();
             capabilities.setCapability("chrome.switches", Arrays.asList("--no-default-browser-check"));
             HashMap<String, String> chromePreferences = new HashMap<String, String>();
             chromePreferences.put("profile.password_manager_enabled", "false");
             capabilities.setCapability("chrome.prefs", chromePreferences);
-            if (proxyEnabled) {
-                capabilities.setCapability(PROXY, getSeleniumProxyDetails());
-            }
-            return capabilities;
+            return addProxySettings(capabilities, proxySettings);
         }
 
         public WebDriver getWebDriverObject(DesiredCapabilities capabilities) {
@@ -62,15 +50,12 @@ public enum DriverType implements DriverSetup {
         }
     },
     IE {
-        public DesiredCapabilities getDesiredCapabilities() {
+        public DesiredCapabilities getDesiredCapabilities(Proxy proxySettings) {
             DesiredCapabilities capabilities = DesiredCapabilities.internetExplorer();
             capabilities.setCapability(CapabilityType.ForSeleniumServer.ENSURING_CLEAN_SESSION, true);
             capabilities.setCapability(InternetExplorerDriver.ENABLE_PERSISTENT_HOVERING, true);
             capabilities.setCapability("requireWindowFocus", true);
-            if (proxyEnabled) {
-                capabilities.setCapability(PROXY, getSeleniumProxyDetails());
-            }
-            return capabilities;
+            return addProxySettings(capabilities, proxySettings);
         }
 
         public WebDriver getWebDriverObject(DesiredCapabilities capabilities) {
@@ -83,13 +68,10 @@ public enum DriverType implements DriverSetup {
         }
     },
     SAFARI {
-        public DesiredCapabilities getDesiredCapabilities() {
+        public DesiredCapabilities getDesiredCapabilities(Proxy proxySettings) {
             DesiredCapabilities capabilities = DesiredCapabilities.safari();
             capabilities.setCapability("safari.cleanSession", true);
-            if (proxyEnabled) {
-                capabilities.setCapability(PROXY, getSeleniumProxyDetails());
-            }
-            return capabilities;
+            return addProxySettings(capabilities, proxySettings);
         }
 
         public WebDriver getWebDriverObject(DesiredCapabilities capabilities) {
@@ -97,12 +79,9 @@ public enum DriverType implements DriverSetup {
         }
     },
     OPERA {
-        public DesiredCapabilities getDesiredCapabilities() {
+        public DesiredCapabilities getDesiredCapabilities(Proxy proxySettings) {
             DesiredCapabilities capabilities = DesiredCapabilities.operaBlink();
-            if (proxyEnabled) {
-                capabilities.setCapability(PROXY, getSeleniumProxyDetails());
-            }
-            return capabilities;
+            return addProxySettings(capabilities, proxySettings);
         }
 
         public WebDriver getWebDriverObject(DesiredCapabilities capabilities) {
@@ -115,19 +94,15 @@ public enum DriverType implements DriverSetup {
         }
     },
     PHANTOMJS {
-        public DesiredCapabilities getDesiredCapabilities() {
+        public DesiredCapabilities getDesiredCapabilities(Proxy proxySettings) {
             DesiredCapabilities capabilities = DesiredCapabilities.phantomjs();
-            capabilities.setCapability("takesScreenshot", true);
             final List<String> cliArguments = new ArrayList<String>();
             cliArguments.add("--web-security=false");
             cliArguments.add("--ssl-protocol=any");
             cliArguments.add("--ignore-ssl-errors=true");
-            if (proxyEnabled) {
-                cliArguments.add("--proxy-type=http");
-                cliArguments.add("--proxy=" + proxyDetails);
-            } else {
-                cliArguments.add("--proxy-type=none");
-            }
+            capabilities.setCapability("phantomjs.cli.args", applyPhantomJSProxySettings(cliArguments, proxySettings));
+            capabilities.setCapability("takesScreenshot", true);
+
             return capabilities;
         }
 
@@ -141,72 +116,26 @@ public enum DriverType implements DriverSetup {
         }
     };
 
-    public static final DriverType defaultDriverType = FIREFOX;
-    public static final boolean useRemoteWebDriver = Boolean.valueOf(System.getProperty("remoteDriver"));
-    private static final String operatingSystem = System.getProperties().getProperty("os.name").toUpperCase();
-    private static final String systemArchitecture = System.getProperties().getProperty("os.arch");
-    public static boolean proxyEnabled = getBoolean("proxyEnabled");
-    private static final String proxyDetails = String.format("%s:%s", System.getProperty("proxy"), System.getProperty("proxyPort"));
-
-    public Proxy getSeleniumProxyDetails() {
-        Proxy proxy = new Proxy();
-        proxy.setProxyType(MANUAL);
-        proxy.setHttpProxy(proxyDetails);
-        proxy.setSslProxy(proxyDetails);
-        return proxy;
-    }
 
     public String getWebDriverSystemPropertyKey() {
         return null;
     }
 
-    public WebDriver instantiateWebDriver() throws MalformedURLException {
-        DesiredCapabilities desiredCapabilities = getDesiredCapabilities();
-
-        if (useRemoteWebDriver) {
-            URL seleniumGridURL = new URL(System.getProperty("gridURL"));
-            String desiredBrowserVersion = System.getProperty("desiredBrowserVersion");
-            String desiredPlatform = System.getProperty("desiredPlatform");
-
-            if (null != desiredPlatform && !desiredPlatform.isEmpty()) {
-                desiredCapabilities.setPlatform(Platform.valueOf(desiredPlatform.toUpperCase()));
-            }
-
-            if (null != desiredBrowserVersion && !desiredBrowserVersion.isEmpty()) {
-                desiredCapabilities.setVersion(desiredBrowserVersion);
-            }
-
-            return new RemoteWebDriver(seleniumGridURL, desiredCapabilities);
+    protected DesiredCapabilities addProxySettings(DesiredCapabilities capabilities, Proxy proxySettings) {
+        if (null != proxySettings) {
+            capabilities.setCapability(PROXY, proxySettings);
         }
 
-        return getWebDriverObject(desiredCapabilities);
+        return capabilities;
     }
 
-    public static DriverType determineEffectiveDriverType(String browser) {
-        DriverType driverType = defaultDriverType;
-        try {
-            driverType = valueOf(browser.toUpperCase());
-        } catch (IllegalArgumentException ignored) {
-            System.err.println("Unknown driver specified, defaulting to '" + driverType + "'...");
-        } catch (NullPointerException ignored) {
-            System.err.println("No driver specified, defaulting to '" + driverType + "'...");
+    protected List<String> applyPhantomJSProxySettings(List<String> cliArguments, Proxy proxySettings) {
+        if (null == proxySettings) {
+            cliArguments.add("--proxy-type=none");
+        } else {
+            cliArguments.add("--proxy-type=http");
+            cliArguments.add("--proxy=" + proxySettings.getHttpProxy());
         }
-
-        return driverType;
-    }
-
-    public WebDriver configureDriverBinaryAndInstantiateWebDriver() {
-        System.out.println(" ");
-        System.out.println("Current Operating System: " + operatingSystem);
-        System.out.println("Current Architecture: " + systemArchitecture);
-        System.out.println("Current Browser Selection: " + this);
-        System.out.println(" ");
-
-        try {
-            return instantiateWebDriver();
-        } catch (MalformedURLException urlIsInvalid) {
-            urlIsInvalid.printStackTrace();
-            return null;
-        }
+        return cliArguments;
     }
 }
