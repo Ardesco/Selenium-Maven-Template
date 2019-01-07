f9b9b3cf3edc6ba1d07a413094b30e6973de3f10
Mark Collin <mark.collin@lazeryattack.com>
2019-01-07 12:58:50 +0000

    Update library dependencies Update Geckodriver download links

--- src/test/java/com/lazerycode/selenium/page_objects/GoogleHomePage.java
+++ src/test/java/com/lazerycode/selenium/page_objects/GoogleHomePage.java
@@ -3,17 +3,17 @@ package com.lazerycode.selenium.page_objects;
 import com.lazerycode.selenium.DriverBase;
 import com.lazerycode.selenium.util.Query;
 import org.openqa.selenium.By;
-import org.openqa.selenium.remote.RemoteWebDriver;
+
+import static com.lazerycode.selenium.util.AssignDriver.initQueryObjects;
 
 public class GoogleHomePage {
 
-    private final RemoteWebDriver driver = DriverBase.getDriver();
-
-    private Query searchBar = new Query(By.name("q"), driver);
-    private Query googleSearch = new Query(By.name("btnK"), driver);
-    private Query imFeelingLucky = new Query(By.name("btnI"), driver);
+    private Query searchBar = new Query().defaultLocator(By.name("q"));
+    private Query googleSearch = new Query().defaultLocator(By.name("btnK"));
+    private Query imFeelingLucky = new Query().defaultLocator(By.name("btnI"));
 
     public GoogleHomePage() throws Exception {
+        initQueryObjects(this, DriverBase.getDriver());
     }
 
     public GoogleHomePage enterSearchTerm(String searchTerm) {
