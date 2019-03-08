95a52ef88a92ce7a43e3f2e1b1f53dd679656cb5
Jyothee Gottam <jgottam@m-c02xd0d6jgh7.homeoffice.wal-mart.com>
2019-03-07 22:38:44 -0800

    this is for urekha demo

--- src/test/java/com/lazerycode/selenium/tests/GoogleExampleIT.java
+++ src/test/java/com/lazerycode/selenium/tests/GoogleExampleIT.java
@@ -42,6 +42,36 @@ public class GoogleExampleIT extends DriverBase {
         System.out.println("Page title is: " + driver.getTitle());
     }
 
+
+    @Test
+    public void demoForUrekha() throws Exception {
+        // Create a new WebDriver instance
+        // Notice that the remainder of the code relies on the interface,
+        // not the implementation.
+        WebDriver driver = getDriver();
+
+        // And now use this to visit Google
+        driver.get("http://www.google.com");
+        // Alternatively the same thing can be done like this
+        // driver.navigate().to("http://www.google.com");
+
+        GoogleHomePage googleHomePage = new GoogleHomePage();
+
+        // Check the title of the page
+        System.out.println("Page title is: " + driver.getTitle());
+
+        googleHomePage.enterSearchTerm("Cheese")
+                .submitSearch();
+
+        // Google's search is rendered dynamically with JavaScript.
+        // Wait for the page to load, timeout after 10 seconds
+        WebDriverWait wait = new WebDriverWait(driver, 10, 100);
+        wait.until(pageTitleStartsWith("Cheese"));
+
+        // Should see: "cheese! - Google Search"
+        System.out.println("Page title is: " + driver.getTitle());
+    }
+
     @Test
     public void googleMilkExample() throws Exception {
         // Create a new WebDriver instance
