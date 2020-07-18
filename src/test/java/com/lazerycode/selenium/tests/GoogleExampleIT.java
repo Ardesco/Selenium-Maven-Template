753f4725a9fdb62da878156941181bacdee377f5
opu.zaman <opu.zaman@shibumi.com>
2020-07-18 12:36:33 -0400

    Added test case

--- src/test/java/com/lazerycode/selenium/tests/GoogleExampleIT.java
+++ src/test/java/com/lazerycode/selenium/tests/GoogleExampleIT.java
@@ -2,8 +2,9 @@ package com.lazerycode.selenium.tests;
 
 import com.lazerycode.selenium.DriverBase;
 import com.lazerycode.selenium.page_objects.GoogleHomePage;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
@@ -14,60 +15,20 @@ public class GoogleExampleIT extends DriverBase {
     }
 
     @Test
-    public void googleCheeseExample() throws Exception {
-        // Create a new WebDriver instance
-        // Notice that the remainder of the code relies on the interface,
-        // not the implementation.
+    public void apple() throws Exception {
         WebDriver driver = getDriver();
+        driver.get("http://www.apple.com");
 
-        // And now use this to visit Google
-        driver.get("http://www.google.com");
-        // Alternatively the same thing can be done like this
-        // driver.navigate().to("http://www.google.com");
-
-        GoogleHomePage googleHomePage = new GoogleHomePage();
-
-        // Check the title of the page
-        System.out.println("Page title is: " + driver.getTitle());
-
-        googleHomePage.enterSearchTerm("Cheese")
-                .submitSearch();
-
-        // Google's search is rendered dynamically with JavaScript.
-        // Wait for the page to load, timeout after 10 seconds
         WebDriverWait wait = new WebDriverWait(driver, 10, 100);
-        wait.until(pageTitleStartsWith("Cheese"));
+        wait.until(pageTitleStartsWith("apple"));
 
         // Should see: "cheese! - Google Search"
         System.out.println("Page title is: " + driver.getTitle());
-    }
 
-    @Test
-    public void googleMilkExample() throws Exception {
-        // Create a new WebDriver instance
-        // Notice that the remainder of the code relies on the interface,
-        // not the implementation.
-        WebDriver driver = getDriver();
+//        driver.findElement(By.cssSelector(".ac-gn-link-bag"));
 
-        // And now use this to visit Google
-        driver.get("http://www.google.com");
-        // Alternatively the same thing can be done like this
-        // driver.navigate().to("http://www.google.com");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/nav/div/ul[2]/li[10]/div/a"))).click();
 
-        GoogleHomePage googleHomePage = new GoogleHomePage();
-
-        // Check the title of the page
-        System.out.println("Page title is: " + driver.getTitle());
-
-        googleHomePage.enterSearchTerm("Milk")
-                .submitSearch();
-
-        // Google's search is rendered dynamically with JavaScript.
-        // Wait for the page to load, timeout after 10 seconds
-        WebDriverWait wait = new WebDriverWait(driver, 10, 100);
-        wait.until(pageTitleStartsWith("Milk"));
-
-        // Should see: "cheese! - Google Search"
-        System.out.println("Page title is: " + driver.getTitle());
+        Thread.sleep(4000);
     }
 }
